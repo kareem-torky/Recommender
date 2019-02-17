b3e37aac2deacc618347211e988045b4cadd0c60
Kareem M. Fouad <kareemmfouad.ece@gmail.com>
2019-02-17 20:46:23 +0200

    8th

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 offset-lg-3 form-box">
+                @include('inc.messages')
                 {!! Form::open(['route'=>'login', 'method'=>'POST']) !!}
         
                     {!! Form::token() !!}
